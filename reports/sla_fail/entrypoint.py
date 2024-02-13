c202d0c7d388b7ddc9051d98eb3c30eb8459d1c3
luismiguelru <luismiguelru@hotmail.com>
2024-02-13 11:15:25 +0100

    SLA Report Automation changes

--- reports/sla_fail/entrypoint.py
+++ reports/sla_fail/entrypoint.py
@@ -4,9 +4,12 @@
 # All rights reserved.
 import datetime
 import logging
-from connect.client import R
+import Levenshtein
 
-from reports.utils import convert_to_datetime, get_dict_element
+from connect.client import R
+from collections import defaultdict
+
+from reports.utils import convert_to_datetime, get_dict_element,convert_list_jira_info,calculate_similarity,check_report_generation
 
 def generate(
     client=None,
@@ -15,6 +18,22 @@ def generate(
     renderer_type=None,
     extra_context_callback=None,
 ):
+    # Initialize a dictionary to store IDs and their associated conversation notes
+    id_conversation_notes_dict = {}
+    try:
+        import requests
+        url = "https://jira.int.zone/rest/api/2/myself"
+        headers = {
+            "Authorization": f"Bearer {parameters['api_token']}"
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+    except requests.HTTPError as e:
+        if response.status_code == 401:
+            raise RuntimeError("Invalid token") from e
+    except Exception as e:
+        raise RuntimeError("Unexpected error occurred") from e
+
     try:
         offset_red = int(parameters['offset_red_days'])
         offset_yellow = int(parameters['offset_yellow_days'])
@@ -48,11 +67,13 @@ def generate(
         'yellow': offset_yellow,
     }
 
+    jira_api_token=parameters["api_token"]
+
+
     for request in requests:
-        yield _process_line(request, levels,client)
+        yield _process_line(request, levels,client,jira_api_token)
         progress += 1
         progress_callback(progress, total)
-
 
 def _get_awaiting_for(data):
     return (datetime.datetime.utcnow() - convert_to_datetime(data['created'])).days
@@ -74,28 +95,117 @@ def _get_sla_level(awaiting_days, levels):
     else:
         return 'GREEN'
 
-def _get_conversation_messages(client,data):
-    first = data['id']
-    messages = client.collection('conversations')[first].collection('messages').all()
+def _get_latest_sla_indicator_message(client,data):
+    conversation_id = data['id']
+    messages = client.collection('conversations')[conversation_id].collection('messages').all()
 
     # Sort the messages based on the 'created' timestamp
     sorted_messages = sorted(messages, key=lambda x: x['created'], reverse=True)
 
-    for real_message in sorted_messages:
-        print("Message Value:", real_message['text'])
+    for message in sorted_messages:
+        if "Indicator of Service Level Agreement" not in message['text']:
+            return message["text"]
+    return None
 
-        if "Indicator of Service Level Agreement" not in real_message['text']:
-            # Print the message and stop the iteration
-            print("Non-Indicator Message:", real_message['text'])
-            break
+grouping_messages_cache = None
 
-    return real_message["text"]
+def _get_grouping_messages(client,jira_api_token):
+    global grouping_messages_cache
+    if grouping_messages_cache is None:
+        grouping_messages_cache = _actual_get_grouping_messages(client,jira_api_token)
+    return grouping_messages_cache
 
+def _actual_get_grouping_messages(client,jira_api_token):
+    pr_request_dict_list = []  # Initialize an empty list for dictionaries
+    pr_request = client.requests.filter(status='pending').all()
 
-def _process_line(data, levels,client):
+    for row in pr_request:
+        message = client.conversations[row['id']].messages.filter(type='message').all()
+
+        for message_value in message:
+            text_message = message_value['text']
+            created_by_name = message_value['events']['created']['by'][
+                'name'] if 'events' in message_value and 'created' in message_value['events'] else None
+
+            if text_message is not None and created_by_name == "Luis Miguel Rodriguez Ugarte":
+                pr_dict = {'ID': row['id'], 'Notes': text_message}
+                pr_request_dict_list.append(pr_dict)
+                break
+    grouped_dict = defaultdict(list)
+    first_common_part = None
+    for pr_dict in pr_request_dict_list:
+        found_group = False
+        similarity_threshold = 0.9  # Adjust the threshold as needed
+
+        for group_notes, group_ids in grouped_dict.items():
+            similarity = calculate_similarity(pr_dict['Notes'], group_notes)
+            if similarity >= similarity_threshold:
+                found_group = True
+                common_part_length = int(min(len(pr_dict['Notes']), len(group_notes)) * similarity)
+                common_part = pr_dict['Notes'][:common_part_length]
+
+                if first_common_part is None:
+                    first_common_part = common_part
+                break
+            # If no similar group is found, create a new group
+        if found_group and common_part:
+            # Append to the existing group with common_part
+            if group_notes != first_common_part:  # Check if key needs to be updated
+                grouped_dict[first_common_part] = grouped_dict.pop(group_notes, [])  # Update key to common_part
+            grouped_dict[first_common_part].append(pr_dict['ID'])  # Append ID to the group
+        else:
+            # Create a new group with the common_part
+            grouped_dict[pr_dict['Notes']].append(pr_dict['ID'])
+
+            # Use the generated report for Jira updates
+
+    report_generation = convert_list_jira_info(grouped_dict, jira_api_token)
+    print(report_generation)
+    check_report_generation(report_generation,client)
+
+    return report_generation
+
+def get_notes_for_id(report_generation, target_id):
+    for item in report_generation:
+        id_list = item['ID'].split(', ')
+        for id_str in id_list:
+            if id_str == target_id:
+                return item.get('Notes', None)
+    return None
+def get_jira_ticket_and_status_for_id(report_generation, target_id):
+    result = []
+    for item in report_generation:
+        id_list = item['ID'].split(', ')
+        if target_id in id_list:
+            index = id_list.index(target_id)
+            jira_ticket = item.get('JIRA TICKET', [])[index] if len(item.get('JIRA TICKET', [])) > index else None
+            jira_status = item.get('JIRA STATUS', [])[index] if len(item.get('JIRA STATUS', [])) > index else None
+            result.append({
+                'JIRA Ticket': jira_ticket,
+                'JIRA Status': jira_status
+            })
+    return result
+
+def get_jira_ticket_for_id(report_generation, target_id):
+    jira_ticket_and_status = get_jira_ticket_and_status_for_id(report_generation, target_id)
+    if jira_ticket_and_status:
+        jira_ticket_list = [item['JIRA Ticket'] for item in jira_ticket_and_status]
+        jira_ticket_str = ', '.join(jira_ticket_list)
+        return jira_ticket_str
+    return None
+
+def get_jira_status_for_id(report_generation, target_id):
+    jira_ticket_and_status = get_jira_ticket_and_status_for_id(report_generation, target_id)
+    if jira_ticket_and_status:
+        jira_status_list = [item['JIRA Status'] for item in jira_ticket_and_status]
+        jira_status_str = ', '.join(jira_status_list)
+        return jira_status_str
+    return None
+def _process_line(data, levels,client,jira_api_token):
     awaiting_for_days = _get_awaiting_for(data)
     sla_level = _get_sla_level(awaiting_for_days, levels)
-    conversation_notes = _get_conversation_messages(client,data)
+    tier_sample = _get_grouping_messages(client,jira_api_token)
+
     return (
         data.get('id'),
         get_dict_element(data, 'asset', 'product', 'id'),
@@ -109,22 +219,8 @@ def _process_line(data, levels,client):
         convert_to_datetime(get_dict_element(data, 'created')),
         get_dict_element(data, 'status'),
         get_dict_element(data, 'asset', 'connection', 'type'),
-        get_dict_element(data, 'assignee', 'email'),
-        get_dict_element(data, 'asset', 'tiers', 'customer', 'id'),
-        get_dict_element(data, 'asset', 'tiers', 'customer', 'external_id'),
-        get_dict_element(data, 'asset', 'tiers', 'customer', 'name'),
-        _get_contact(data['asset']['tiers'].get('customer')),
-        get_dict_element(data, 'asset', 'tiers', 'customer', 'contact_info', 'contact', 'email'),
-        get_dict_element(data, 'asset', 'tiers', 'tier1', 'name'),
-        get_dict_element(data, 'asset', 'tiers', 'tier1', 'external_id'),
-        _get_contact(data['asset']['tiers'].get('tier1')),
-        get_dict_element(data, 'asset', 'tiers', 'tier1', 'contact_info', 'contact', 'email'),
-        get_dict_element(data, 'asset', 'tiers', 'tier2', 'name'),
-        get_dict_element(data, 'asset', 'tiers', 'tier2', 'external_id'),
-        _get_contact(data['asset']['tiers'].get('tier2')),
-        get_dict_element(data, 'asset', 'tiers', 'tier2', 'contact_info', 'contact', 'email'),
-        get_dict_element(data, 'marketplace', 'id'),
-        get_dict_element(data, 'marketplace', 'name'),
         sla_level,
-        conversation_notes,
+        get_notes_for_id(tier_sample,data.get('id')),
+        get_jira_ticket_for_id(tier_sample,data.get('id')),
+        get_jira_status_for_id(tier_sample,data.get('id')),
     )
--- reports/utils.py
+++ reports/utils.py
@@ -7,7 +7,9 @@
 from datetime import datetime
 from threading import Lock
 import collections
-
+import requests
+import Levenshtein
+import re
 
 def convert_to_datetime(param_value):
     if param_value == "" or param_value == "-":
@@ -56,3 +58,160 @@ def get_dict_element(dictionary, *keys):
         return dictionary
     key = keys[0]
     return get_dict_element(dictionary[key], *keys[1:])
+
+def convert_list_jira_info(data,jira_api_token):
+    result_list = []
+    for notes, ids in data.items():
+        new_data = {'ID': ', '.join(ids), 'Notes': notes}
+        result_list.append(update_jira_info(new_data,jira_api_token))
+    return result_list
+def update_jira_info(data,jira_api_token):
+
+    all_ids = data['ID'].split(', ')
+    jira_tickets = {}
+    jira_statuses = {}
+    ids_not_in_jira = []
+
+    for id_str in all_ids:
+        id_str = id_str.strip()
+        id_match = re.match(r'PR-\d{4}-\d{4}-\d{4}-\d{3}', id_str)
+
+        if not id_match:
+            print(f"Invalid ID format: {id_str}")
+            continue
+        jira_ticket, jira_status = search_in_jira(id_str,jira_api_token)
+
+
+        if jira_ticket == 'No ticket':
+            ids_not_in_jira.append(id_str)
+            jira_tickets[id_str] = None
+            jira_statuses[id_str] = None
+        else:
+            jira_tickets[id_str] = jira_ticket
+            jira_statuses[id_str] = jira_status
+
+
+    if ids_not_in_jira:
+            # Create a Jira issue for IDs that do not exist in Jira
+        created_tickets = create_jira_issue(data['Notes'], ids_not_in_jira,jira_api_token)
+
+        if created_tickets:
+            # Update jira_tickets and jira_statuses based on created_tickets information
+            for ticket_key in created_tickets:
+                for id_str in ids_not_in_jira:
+                    jira_tickets[id_str] = ticket_key
+                    jira_statuses[id_str] = 'Open'
+
+        # If all IDs exist in Jira, print the ticket number; otherwise, print 'No ticket'
+    data['JIRA TICKET'] = [jira_tickets[id_str] for id_str in all_ids] if all(
+        ticket != 'No ticket' for ticket in jira_tickets.values()) else ['No ticket']
+    data['JIRA STATUS'] = [jira_statuses[id_str] for id_str in all_ids] if all(
+        status is not None for status in jira_statuses.values()) else ['N/A']
+
+    return data
+
+def search_in_jira(pr_id,jira_api_token):
+    # Replace this with your actual JIRA API endpoint and authentication method
+    jira_url = "https://jira.int.zone/rest/api/2/search"
+    headers = {
+        "Authorization": f"Bearer {jira_api_token}",
+        "Content-Type": "application/json"
+    }
+    search_word = pr_id
+
+    query = {
+        'jql': f'text ~ "{search_word}"'
+    }
+
+    try:
+        response = requests.post(jira_url, headers=headers, json=query)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+        issues = response.json()
+
+        if 'issues' in issues and issues['issues']:
+            # get details from the first issue if there are multiple matches
+            issue = issues['issues'][0]
+            jira_ticket = issue['key']
+            jira_status = issue['fields'].get('status', {}).get('name', '')
+            return jira_ticket, jira_status
+        else:
+            return 'No ticket', 'N/A'
+    except requests.exceptions.RequestException as e:
+        print("Error occurred while searching issues:", e)
+        return 'No ticket', 'N/A'
+
+def create_jira_issue(notes, ids_not_in_jira,jira_api_token):
+    jira_url = "https://jira.int.zone/rest/api/2/issue"
+
+    headers = {
+        "Authorization": f"Bearer {jira_api_token}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "fields": {
+            "summary": "Ticket created by SLA Report Automation",
+            "issuetype": {
+                "name": "3rd-line Ticket"
+            },
+            "duedate": "2024-10-28",
+            "project": {
+                "key": "TRITS"
+            },
+            "description": f"This is being created automatically by SLA Report Automation. The reason: {notes}\nID(s): {', '.join(ids_not_in_jira)}",
+        }
+    }
+
+    response = requests.post(jira_url, headers=headers, json=data)
+
+    if response.status_code == 201:
+        print(f"Issue created successfully for IDs: {', '.join(ids_not_in_jira)}")
+        print("Issue Key:", response.json()["key"])
+        return [response.json()["key"]]
+    else:
+        print(f"Failed to create issue for IDs: {', '.join(ids_not_in_jira)}. Status code: {response.status_code}")
+        print("Response content:", response.text)
+        return [response.json()["key"]]
+
+
+def calculate_similarity(str1, str2):
+    len_max = max(len(str1), len(str2))
+    if len_max == 0:
+        return 0.0
+    distance = Levenshtein.distance(str1, str2)
+    similarity = 1.0 - distance / len_max
+    return similarity
+
+def check_report_generation(report,client):
+    for item in report:
+        id_list = item['ID'].split(', ')
+        jira_tickets_str = item.get('JIRA TICKET', "")
+
+        # Ensure jira_tickets_str is a string
+        if isinstance(jira_tickets_str, list):
+            jira_tickets_str = ', '.join(jira_tickets_str)
+        jira_tickets = [ticket.strip() for ticket in jira_tickets_str.split(',')] if jira_tickets_str else []
+
+        for id_str in id_list:
+            jira_ticket_found = False
+
+            for jira_ticket in jira_tickets:
+                 # Replace conversation_id with the actual conversation ID from your data
+                messages = client.conversations[id_str].messages.filter(type='message').all().values_list(
+                    'id', 'type', 'text',
+                )
+
+                for message_value in messages:
+                    text_message = message_value['text']
+
+                    if jira_ticket in text_message:
+                        jira_ticket_found = True
+                        break
+
+                if jira_ticket_found:
+                    break
+
+            if not jira_ticket_found:
+                message_data = {"text": f"We have created a ticket {jira_ticket}"}
+                client.conversations[id_str].messages.create(json=message_data)
+    return report
