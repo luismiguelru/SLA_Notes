7264de09432bad817423b527377bc427aca73eb5
luismiguelru <luismiguelru@hotmail.com>
2024-02-14 11:43:11 +0100

    SLA Report Automation changes

--- reports/utils.py
+++ reports/utils.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from threading import Lock
 import collections
 import requests
-import Levenshtein
+#import Levenshtein
 import re
 
 def convert_to_datetime(param_value):
@@ -173,12 +173,31 @@ def create_jira_issue(notes, ids_not_in_jira,jira_api_token):
         print("Response content:", response.text)
         return [response.json()["key"]]
 
+def levenshtein_distance(str1, str2):
+    if len(str1) < len(str2):
+        return levenshtein_distance(str2, str1)
+
+    if len(str2) == 0:
+        return len(str1)
+
+    previous_row = range(len(str2) + 1)
+
+    for i, c1 in enumerate(str1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(str2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
 
 def calculate_similarity(str1, str2):
     len_max = max(len(str1), len(str2))
     if len_max == 0:
         return 0.0
-    distance = Levenshtein.distance(str1, str2)
+    distance = levenshtein_distance(str1, str2)
     similarity = 1.0 - distance / len_max
     return similarity
 
